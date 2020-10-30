2b4a595bd74700259ff14365a663dc62691076d8
reticenceji <1710940252@qq.com>
2020-10-30 10:27:04 +0800

    保存了CSR，正确的处理了mip

--- arch/riscv/kernel/strap.c
+++ arch/riscv/kernel/strap.c
@@ -4,7 +4,7 @@ void sti_handler_s()
 {
 	const static char prompt[]="\n[S] Supervisor Mode Timer Interrupt ";
 	static int count=0,if_handred=0;		//可以关注一下这两个被分配到哪个段了
-	if (if_handred==100)		//100个好快。。
+	if (if_handred==100)	
 	{
 		puts(prompt);
 		puti(count);
