5267a742651f3808bb17e1a0a1153891bff00d87
RegulusGuo <ruorong.guo@gmail.com>
2020-11-04 02:17:13 +0800

    complete <tast_init> but not sure, can make successfully with a warning

--- arch/riscv/kernel/sched.c
+++ arch/riscv/kernel/sched.c
@@ -1,7 +1,45 @@
 #include "sched.h"
+struct task_struct *current;
+struct task_struct *task[NR_TASKS];
 
 /* 进程初始化 创建四个dead_loop进程 */ 
-void task_init(void); 
+void task_init(void)
+{
+    current = (struct task_struct*)0x80010000;
+    task[0] = current;
+    task[0]->state = TASK_RUNNING;
+    task[0]->counter = 0;
+    task[0]->priority = 5;
+    task[0]->blocked = 0;
+    task[0]->pid = 0;
+    task[0]->thread.sp = (unsigned long long)task[0];
+    
+    #ifdef SJF
+    for (int i=1; i < 5; i++)
+    {
+        task[i] = (struct task_struct*)(0x80010000 + i*0x1000);
+        task[i]->state = TASK_RUNNING;
+        task[i]->counter = rand();
+        task[i]->priority = 5;
+        task[i]->blocked = 0;
+        task[i]->pid = i;
+        task[i]->thread.sp = (unsigned long long)task[i] + 0x1000;
+    }
+    #endif
+
+    #ifdef PRIORITY
+    for (int i=1; i < 5; i++)
+    {
+        task[i] = (struct task_struct*)(0x80010000 + i*0x1000);
+        task[i]->state = TASK_RUNNING;
+        task[i]->counter = 8-i;
+        task[i]->priority = 5;
+        task[i]->blocked = 0;
+        task[i]->pid = i;
+        task[i]->thread.sp = (unsigned long long)task[i] + 0x1000;
+    }
+    #endif
+}
 
 /* 在时钟中断处理中被调用 */
 void do_timer(void);
