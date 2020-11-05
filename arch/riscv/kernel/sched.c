d0eceb68138f126083fd3c57fb87f2535993b49e
RegulusGuo <ruorong.guo@gmail.com>
2020-11-05 11:43:58 +0800

    update sched.c, basically complete the SJF part in <schedule> and <do_timer>

--- arch/riscv/kernel/sched.c
+++ arch/riscv/kernel/sched.c
@@ -1,4 +1,8 @@
 #include "sched.h"
+#include "../../../include/rand.h"
+
+#define INF 1e9
+
 struct task_struct *current;
 struct task_struct *task[NR_TASKS];
 
@@ -12,10 +16,10 @@ void task_init(void)
     task[0]->priority = 5;
     task[0]->blocked = 0;
     task[0]->pid = 0;
-    task[0]->thread.sp = (unsigned long long)task[0];
+    task[0]->thread.sp = (unsigned long long)task[0] + TASK_SIZE;
     
     #ifdef SJF
-    for (int i=1; i < 5; i++)
+    for (long long i=1; i < 5; i++)
     {
         task[i] = (struct task_struct*)(0x80010000 + i*TASK_SIZE);
         task[i]->state = TASK_RUNNING;
@@ -28,7 +32,7 @@ void task_init(void)
     #endif
 
     #ifdef PRIORITY
-    for (int i=1; i < 5; i++)
+    for (long long i=1; i < 5; i++)
     {
         task[i] = (struct task_struct*)(0x80010000 + i*TASK_SIZE);
         task[i]->state = TASK_RUNNING;
@@ -42,26 +46,74 @@ void task_init(void)
 }
 
 /* 在时钟中断处理中被调用 */
-void do_timer(void);
+void do_timer(void)
+{
+    (current->counter)--;
 
-#ifdef SJF
-/* 短作业优先式调度程序 */
+    #ifdef SJF
+    if (current->counter == 0)
+    {
+        schedule();
+    }
+    return ;
+    #endif
+
+    #ifdef PRIORITY
+
+    #endif
+}
+
 void schedule(void)
 {
+    #ifdef SJF
+    /* 短作业优先式调度程序 */
+    struct task_struct **p;
+    long next;
+    while (1)
+    {
+        long cnt = (*p)->counter, i = LAB_TEST_NUM;
+        p = &task[LAB_TEST_NUM];
+        next = 0;
+        while (--i)
+        {
+            if (!*--p)
+                continue;
+            //判断是否满足运行条件
+            if ((*p)->state == TASK_RUNNING && (*p)->counter > 0 && (*p)->counter <= cnt)
+            {
+                cnt = (*p)->counter;
+                next = i;
+            }
+        }
+        //如果cnt和next都没有被更新过，即没有符合要求的进程 (本实验中即所有counter均为0)
+        //如果task[LAB_TEST_NUM-1]是应该被运行的进程，那么next会被更新，不会触发该条件
+        if (cnt == task[LAB_TEST_NUM-1]->counter && next == 0)
+        {
+            for (int j = LAB_TEST_NUM-1; j > 0; j--)
+            {
+                task[j]->counter = rand();      //重新赋值
+            }
+        }
+        else
+        {
+            break;
+        }
+    }
+    #endif
 
+    #ifdef PROPRITY
+    /* 优先级抢占式 */
+
+    #endif
+
+    switch_to(task[next]);                      //切换到新任务
 }
-#endif
-
-#ifdef PROPRITY
-/* 优先级抢占式 */
-void schedule(void)
-{
-
-}
-#endif
 
 /* 切换当前任务current到下一个任务next */
-void switch_to(struct task_struct* next);
+void switch_to(struct task_struct* next)
+{
+    return ;
+}
 
 /* 死循环 */
 void dead_loop(void);
