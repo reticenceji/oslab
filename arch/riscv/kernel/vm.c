3e6cbf855d63ffa542ffd1633da5d811b9fe7965
reticenceji <reticenceji@qq.com>
2020-12-07 16:47:32 +0800

    update README, create_mapping, but need debug

--- arch/riscv/kernel/vm.c
+++ arch/riscv/kernel/vm.c
@@ -1,6 +1,7 @@
 #include "vm.h"
 
-frame_queue_t *fq;
+static void add_entry(uint64 page_addr,uint64 entry_addr,int number,int permisson);
+static frame_queue_t *fq;
 
 int fq_is_full(frame_queue_t *fq)
 {
@@ -15,9 +16,10 @@ int fq_is_empty(frame_queue_t *fq)
 void init_frame_queue(frame_queue_t *fq)
 {
     fq->front = 0;
-    fq->rear = 0;
     fq->capacity = TEST_FRAME_NUM + 1;
     // TODO the elements in fq->frame haven't been initialized yet
+    for (fq->rear=0;fq->rear<TEST_FRAME_NUM;fq->rear++)
+        fq->frame[fq->rear] = KERNEL_START_P + FRAME_SIZE*fq->rear;
 }
 
 uint64 alloc_frame(frame_queue_t *fq)
@@ -58,4 +60,31 @@ uint64* paging_init()
 int create_mapping(uint64 *pgtbl, uint64 va, uint64 pa, uint64 sz, int perm)
 {
     // TODO 
+    /* 分别是页表数量，三级页表数量，二级页表数量 */
+    int num_pd,num_pmd,num_pud; 
+    int i,j,k;
+    uint64 addr_pud,addr_pmd,addr_pd;
+    num_pd = (sz+PAGE_SIZE-1) << 12;
+    num_pmd = (num_pd+ENTRY_PER_PAGE-1) << 9;
+    num_pud = (num_pd+ENTRY_PER_PAGE-1) << 9;
+    for (i=0;i<num_pud;i++)
+    {
+        addr_pud = alloc_frame(fq);
+        add_entry((uint64)pgtbl,addr_pud,i,perm);
+        for (j=0;j<num_pmd;j++)
+        {
+            addr_pmd = alloc_frame(fq);
+            add_entry(addr_pud,addr_pmd,j,perm);
+            for (k=0;k<num_pd;k++)
+            {
+                addr_pd = alloc_frame(fq);
+                add_entry(addr_pmd,addr_pd,j,perm);
+            }
+        }
+    }
 }
+
+static void add_entry(uint64 page_addr,uint64 entry_addr,int number,int permisson)
+{
+    *(uint64 *)(page_addr + (uint64)number*8) = (entry_addr >> 12 << 12) + permisson;
+} 
