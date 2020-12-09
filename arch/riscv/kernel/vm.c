022da403840ebf68e87f7c775c9487dc7ed80660
reticenceji <reticenceji@qq.com>
2020-12-09 17:28:17 +0800

    修改了create_mapping，生成的页表应该没有问题

--- arch/riscv/kernel/vm.c
+++ arch/riscv/kernel/vm.c
@@ -30,7 +30,6 @@ void init_frame_queue(frame_queue_t *fq)
 {
     fq->front = 0;
     fq->capacity = TEST_FRAME_NUM + 1;
-    // TODO the elements in fq->frame haven't been initialized yet
     for (fq->rear=0;fq->rear<TEST_FRAME_NUM;fq->rear++)
         fq->frame[fq->rear] = KERNEL_START_P + KERNEL_SIZE + FRAME_SIZE*fq->rear;
 }
@@ -59,7 +58,7 @@ void free_frame(frame_queue_t *fq)
 __attribute__((optimize("O0"))) uint64* paging_init()
 {
     #ifdef DEBUG
-    print("[*] Function paging_init\n[info] frame_queue:%X\n",&frame_queue);
+    print("[*]\tFunction paging_init\n[info] frame_queue:%X\n",&frame_queue);
     #endif
     uint64* page_base;
     init_frame_queue(&frame_queue);
@@ -70,6 +69,9 @@ __attribute__((optimize("O0"))) uint64* paging_init()
     //create_mapping(page_base, KERNEL_START_V, KERNEL_START_P, KERNEL_SIZE, FLAG_U|FLAG_R|FLAG_W|FLAG_X);
     //低地址的等值映射,可以理解为是那些外部设备map到内存的地方.
     create_mapping(page_base, UART_START, UART_START,UART_SIZE, FLAG_U|FLAG_R|FLAG_W|FLAG_X);
+    #ifdef DEBUG
+    print("[*]\tFunction paging_init DONE\n");  
+    #endif
     return page_base;
 }
 
@@ -78,6 +80,8 @@ __attribute__((optimize("O0"))) int create_mapping(uint64 *pgtbl, uint64 va, uin
     // TODO 
     /* 分别是页表条目数量，三级页表数量，二级页表数量 */
     int num_pd, num_pmd, num_pud;
+    /* 最后一张二级页表可能不能被三级页表填满，最后一张三级页表可能不能被entry填满 */
+    int jmax, kmax;
     uint64 addr_pd, addr_pmd, addr_pud;
     int i, j, k;
     int vpn2, vpn1, vpn0;
@@ -85,25 +89,25 @@ __attribute__((optimize("O0"))) int create_mapping(uint64 *pgtbl, uint64 va, uin
     num_pd = (sz + PAGE_SIZE-1) >> 12;
     num_pmd = (num_pd + ENTRY_PER_PAGE-1) >> 9;
     num_pud = (num_pmd + ENTRY_PER_PAGE-1) >> 9;
-
     #ifdef DEBUG
         print("[*] Function create_mapping START\n");
         print("pgtbl=%X, va=%X, pa=%X\n", pgtbl, va, pa);
-        print("[info]\tsz=%X, pmd=%X, pud=%X, pd=%X\n", sz, num_pmd, num_pud, num_pd);
+        print("[info]\tsz=%X, pud=%X, pmd=%X, pd=%X\n", sz, num_pud, num_pmd, num_pd);
     #endif
     for (i = 0; i < num_pud; i++)
     {
         addr_pud = alloc_frame(&frame_queue);
         vpn2 = (va >> 30) & 0x1FF;
         add_entry((uint64)pgtbl, vpn2, addr_pud, perm);
+        jmax = (i==num_pud-1)?((num_pmd-1)&0x1FF)+1:ENTRY_PER_PAGE;
 
-        for (j = 0; j < num_pmd; j++)
+        for (j = 0; j < jmax; j++)
         {
             addr_pmd = alloc_frame(&frame_queue);
             vpn1 = (va >> 21) & 0x1FF;
             add_entry(addr_pud, vpn1, addr_pmd, perm);
-
-            for (k = 0; k < ENTRY_PER_PAGE; k++)
+            kmax = (i==num_pud-1 && j==jmax-1)?((num_pd-1)&0x1FF)+1:ENTRY_PER_PAGE;
+            for (k = 0; k < kmax; k++)
             {
                 vpn0 = (va >> 12) & 0x1FF;
                 add_entry(addr_pmd, vpn0, pa, perm);
