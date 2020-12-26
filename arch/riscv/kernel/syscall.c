8f0c16246883b06d4841e85f3c605ec06b6872d8
BetaCat <60107867+timmywanttolearn@users.noreply.github.com>
2020-12-26 11:38:37 +0800

    增添了sys_write函数

--- arch/riscv/kernel/syscall.c
+++ arch/riscv/kernel/syscall.c
@@ -1,10 +1,24 @@
-#include "sched.h"
+#include "put.h"
 #include "syscall.h"
+#define NR_OPEN 2//mostly open one case
+
+
 long sys_getpid()
 {
     return getpid();
 }
 long sys_write(unsigned int fd, const char* buf, size_t count)
 {
+//sys_write(unsigned int fd, const char* buf, size_t count)
+    if(fd>=NR_OPEN||count<0)
+    return -1; //wrong
+    if(!count)
     return 0;
+    if(fd==1)
+    {
+        print("%s",buf);
+        return count-1;//dismiss '\n'
+    }
+
+    
 }
