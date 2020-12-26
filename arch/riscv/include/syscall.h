6fe9b547f3902b9a23b08c01370669ef37793e49
BetaCat <60107867+timmywanttolearn@users.noreply.github.com>
2020-12-26 11:43:27 +0800

    设置NR_OPEN，防止fd大于2

--- arch/riscv/include/syscall.h
+++ arch/riscv/include/syscall.h
@@ -3,6 +3,7 @@ typedef unsigned long long uintptr_t ;
 //系统调用号
 #define SYS_WRITE 64
 #define SYS_GETPID 172
+#define NR_OPEN 2
 
 long sys_write(unsigned int fd, const char* buf, size_t count);
 long sys_getpid();
