4cf386abab5801e6ac4a5d5fd3821c3dc1dad9d1
reticenceji <reticenceji@qq.com>
2021-01-12 14:47:15 +0000

    add mprotect.c

--- arch/riscv/kernel/mprotect.c
+++ arch/riscv/kernel/mprotect.c
@@ -1,43 +1,88 @@
 #include "types.h"
+#include "sched.h"
 #include "vm.h"
-#define PAGE_MASK (0xFFF)
-extern struct task_struct *current;
-
-static void change_protection(uint64 va_pte)
-{
-    *(uint64 *)va_pte = 
-}
-
-/* 根据所给的virtual address判断属于哪一个vma */
-struct vm_area_struct *find_vma(uint64 va)
-{
-    struct vm_area_struct *ptr = current->mm->mmap;
-    while (ptr!=NULL)
-    {
-        if (ptr->vm_start <= va && va<ptr->vm_end)
-            return ptr;
-        ptr = ptr->next;
-    }
-    return ptr;
-}
+#include "vma.h"
+#define PAGE_MASK (0x3FF)
+#define FLAG2PAGE_PROT(FLAG) (((FLAG)&0x7)>>1)
 
 /* 检查从start到end的所有内容是不是已经被映射了 */
 static int test_range(uint64 start,uint64 end,struct vm_area_struct *vma)
 {
-
+    struct vm_area_struct *vma,*ins_vma;
+    uint64 tmp_end;
+    for (;start<end;start = vma->vm_end)
+    {
+        vma = vma_find(start);
+        if (vma==NULL) return 0;
+    }
+    return 1;
 }
 
-static void edit_vma(uint64 start,uint64 end)
+/* 修改从start到end的映射，前提是虚拟地址已经连续映射*/
+static inline void edit_vma(uint64 start,uint64 end,int protection)
 {
-    struct vm_area_struct *vma;
-    vma = find_vma(start);
-    if (vma->vm_start != start)
-        
+    struct vm_area_struct *vma,*ins_vma;
+    uint64 tmp_end;
+    vma = vma_find(start);
+    if (vma->vm_start != start) 
+    {
+        ins_vma = (struct vm_area_struct *)kmolloc(sizeof(struct vm_area_struct));
+        ins_vma->vm_start = vm->start;
+        ins_vma->vm_end = start;
+        ins_vma->flags = vma->vm_flags;
+        ins_vma->vm_page_prot = vma->vm_page_prot;
+        ins_vma->mm_struct = current->mm;
+        ins_vma->vm_next = NULL;
+        ins_vma->vm_prev = NULL;
+        insert(ins_vma);
+    }
+    while (1)
+    {
+        tmp_end = vma->vm_end;
+        if (tmp_end>=end) break;
+        delete(vma);
+        vma=vma_find(tmp_end);
+    }
+    ins_vma = (struct vm_area_struct *)kmolloc(sizeof(struct vm_area_struct));
+    ins_vma->vm_start = start;
+    ins_vma->vm_end = end;
+    ins_vma->flags = protection;
+    ins_vma->vm_page_prot = FLAG2PAGE_PROT(protection);
+    ins_vma->mm_struct = current->mm;
+    ins_vma->vm_next = NULL;
+    ins_vma->vm_prev = NULL;
+    insert(ins_vma);
+
+    if (vma->vm_end != tmp_end)
+    {
+        ins_vma = (struct vm_area_struct *)kmolloc(sizeof(struct vm_area_struct));
+        ins_vma->vm_start = end;
+        ins_vma->vm_end = vma->vm_end;
+        ins_vma->flags = vma->vm_flags;
+        ins_vma->vm_page_prot = vma->vm_page_prot;
+        ins_vma->mm_struct = current->mm;
+        ins_vma->vm_next = NULL;
+        ins_vma->vm_prev = NULL;
+        insert(ins_vma);
+    }
 }
 
 /* 从va_start到va_end的每一个页表项都执行func的操作 */
-static inline int edit_page_table(uint64 va_start,uint64 va_end, void func(uint64 va_pte));
-
+static inline void edit_page_table(uint64 va_start,uint64 va_end, int protection)
+{
+    uint64* base =  VP(current->mm->satp & ~PAGE_MASK); //一级页表base
+    uint64* va;
+    int index;
+    for (va = va_start;va<va_end;va+=PAGE_SIZE)
+    {
+        index = (va>>30) & 0x1FF;
+        base = VP(*(base[index]>>10);    //二级页表base
+        index = (va>>21) & 0x1FF;
+        base = VP(*(base[index]>>10);    //三级页表base
+        index = (va>>12) & 0x1FF;               
+        base[index] = base[index] & 0xE | protection ;  //重新修改权限位
+    }
+}
 
 /* 参考Linux1.2.13 的实现
  * 检查参数的合法性
@@ -56,12 +101,13 @@ int mprotect (void *__addr, size_t __len, int __prot)
     start = __addr;
     end = __addr+end;
     /* overflow, 或者试图修改别的位 */
-    if (end<start || __prot&(FLAG_R|FLAG_W|FLAG_X)) 
+    if (end<start || __prot&(0xF)) 
         return -1;          
     /* 中间存在没有映射的区域 */
     if (test_range(start,end,current->vma))
         return -1;
 
-    edit_vma();
-    edit_page_table(start,end,);
+    edit_vma(start,end,__prot);
+    edit_page_table(start,end,FLAG2PAGE_PROT(__prot));
+    return 0;
 }
