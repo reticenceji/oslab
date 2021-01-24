097f97a6fc4eebffe2ae32e7fe27b472c2666bb4
reticenceji <reticenceji@qq.com>
2021-01-24 21:29:54 +0800

    在fork中添加了对sscratch的赋值

--- arch/riscv/kernel/fork.c
+++ arch/riscv/kernel/fork.c
@@ -32,7 +32,7 @@ static struct task_struct* dup_task_struct (struct task_struct *current)
     child->user_sp = kmalloc(PAGE_SIZE);  //user stack
     memmove(child->user_sp,current->stack,PAGE_SIZE);
     child->thread.sp = (uint64)child+PAGE_SIZE; //kernel stack
-    child->sscratch = USER_STACK_BOTTOM_V;
+    child->sscratch = __get_sscratch();
     child->thread.ra = (uint64)fork_ret;
     return child;
 }
