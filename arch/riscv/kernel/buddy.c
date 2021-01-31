54ed2ee6dead0fe0877a2b180e6ea4bcf22f2ba8
reticenceji <reticenceji@qq.com>
2021-01-31 17:02:26 +0800

    重构了fork，修改了注释

--- arch/riscv/kernel/buddy.c
+++ arch/riscv/kernel/buddy.c
@@ -1,4 +1,3 @@
-#include "stddef.h"
 #include "buddy.h"
 #include "vm.h"
 
--- arch/riscv/kernel/fork.c
+++ arch/riscv/kernel/fork.c
@@ -5,51 +5,49 @@
 #include "vma.h"
 #include "vm.h"
 
-static void copy_mm(struct task_struct *child);
-static void copy_mmap(struct task_struct *child);
-static struct task_struct* dup_task_struct (struct task_struct *current);
 extern void ret_from_fork(uint64 *stack,uint64* kernel_sp);
+static void dup_mm(struct task_struct *child);
+static struct task_struct* dup_task_struct (struct task_struct *current);
 
-/* TODO 为子进程创建它的task_struct(包括Stack的整个Page，并修改部分内容。如果创建失败（NR_TASK）返回NULL，否则返回它的地址 
+/* 为子进程创建它的task_struct(包括Stack的整个Page，并修改部分内容。
  * stack内容
+ * TODO kernel部分映射发生改变的情况没有考虑
  */
-static void copy_mm(struct task_struct *child)
+static void dup_mm(struct task_struct *child)
 {
     uint64 pgtbl;
     child->mm = (struct mm_struct *)kmalloc(sizeof(struct mm_struct));
+
     pgtbl = kmalloc(PAGE_SIZE);
-    child->mm->satp = (PP(pgtbl)>>12)|MODE_SV39; //新开页表
+    child->mm->satp = PGTBL2SATP(PP(pgtbl)); //新开页表
     kernel_mapping((uint64 *)pgtbl);    //映射kernel部分，按理来说是复制父进程的？这样子没有考虑kernel部分的映射发生改变的情况，也就是第一个TA说的。
-    //copy_mmap(child);//复制
+    
     vma_copy(child->mm);
 }
 
 static struct task_struct* dup_task_struct (struct task_struct *current)
 {
     struct task_struct *child = (struct task_struct *)kmalloc(TASK_SIZE);
-    memmove(child,current,TASK_SIZE);   //string.h里给的，能用吗，还是要自己写一个？
-    child->pid = newpid();//可以给pid创建一个bitmap标记使用
+    memmove(child,current,TASK_SIZE);   
+    child->pid = newpid();
     child->user_sp = kmalloc(PAGE_SIZE);  //user stack PA
     memmove(child->user_sp,USER_STACK_TOP_V,PAGE_SIZE);
     child->thread.sp = (uint64)child+PAGE_SIZE; //kernel stack
     child->sscratch = __get_sscratch();     //user stack VA
     child->thread.ra = (uint64)fork_ret;
+    child->stack = kmalloc(PT_REGS_SIZE);
+    memmove(child->stack,current->stack,PT_REGS_SIZE);
     return child;
 }
 
-/* TODO 对于父进程，返回子进程的PID；对于子进程，返回0 */
+/* output:对于父进程，返回子进程的PID；对于子进程，返回0，不过不是在此处返回。
+ * 要完整的拷贝父进程的内容，以及修改必要的内容
+ */
 pid_t fork()
 {
     struct task_struct *child_ts;
     child_ts =  dup_task_struct(current);
-    copy_mm(child_ts);
-    child_ts->stack = kmalloc(PT_REGS_SIZE);
-    memmove(child_ts->stack,current->stack,PT_REGS_SIZE);
-    /* 复制用户空间的内容
-     * 代码段、数据段不用，写死在0x84000000，不管他? 这个需要吗？
-     * 堆栈需要另外开一段，并且复制parent堆栈的内容。并且我们需要保存他在task——struct的某个地方。
-     * 创建页表
-     */
+    dup_mm(child_ts);
     task[child_ts->pid] = child_ts;
     print("[PID = %d] Process fork from [PID = %d] Successfully! counter = %d\n",child_ts->pid,current->pid,current->counter);
     return child_ts->pid;
